3f34db3d83facf7e04fd0d7933fa18e485d1abbb
Rajan <rajanvasani9@gmail.com>
2026-06-16 17:23:03 +0530

    chore: build error fixes

--- src/routes/results.routes.js
+++ src/routes/results.routes.js
@@ -4,10 +4,7 @@ const express = require('express');
 const router = express.Router();
 const { authenticate, authorizeRoles } = require('../middlewares/auth.middleware');
 const { getResults, getResultById } = require('../controllers/results.controller');
-const { exportResultById, exportResults } = require('../controllers/results.export.controller');
 
 router.get('/', authenticate, authorizeRoles('Admin', 'Supervisor'), getResults);
 router.get('/:result_id', authenticate, authorizeRoles('Admin', 'Supervisor'), getResultById);
-router.get('/:result_id/export', authenticate, exportResultById);
-router.post('/export', authenticate, exportResults);
 module.exports = router;
